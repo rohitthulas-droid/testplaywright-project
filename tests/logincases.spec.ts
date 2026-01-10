1ccf79779db5cc1e646997001c7d93f5337e4e7c
Your Name <your.email@example.com>
2026-01-10 13:08:08 +0530

    Negative cases added and validations added in login

--- tests/logincases.spec.ts
+++ tests/logincases.spec.ts
@@ -5,12 +5,13 @@ test('Test 1 - Login with valid Data', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.login('student', 'Password123');
+    await expect(page.getByText('Logged In Successfully')).toBeVisible();
   
 });
 
-//test('Test 2 - Login with Invalid Data', async ({ page }) => {
- // const loginPage = new LoginPage(page);
+test('Test 2 - Login with Invalid Data', async ({ page }) => {
+  const loginPage = new LoginPage(page);
 
-  //await loginPage.login('dummdy@aol.com', 'Change_Me@');
-  //await expect(page.getByText('John Doe')).not.toBeVisible();
-//});
+  await loginPage.login('sampleee', 'Change_Me@');
+  await expect(page.getByText('Logged In Successfully')).not.toBeVisible();
+});
